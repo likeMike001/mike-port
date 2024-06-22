e07fb77b0ec5b87c71df6ef60c80f3a51356e477
likeMike001 <like.mike.007.382@gmail.com>
2024-06-22 05:54:15 -0700

    added some custom fonts and started with the about component

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -3,14 +3,14 @@ import crwn from "../../assets/crown.jpg";
 
 const About = () => {
     return (
-      <div className="flex items-center p-4">
+      <div className="flex items-center p-20 -mt-20">
         <img
-          className="w-60 h-auto mr-4"
+          className="w-70 h-auto mr-4 "
           src={crwn}
           alt="Crown"
         />
-        <p className="text-blue-500">
-          Hey, this is ArB (AKA LikeMike), having keen interest in Data Structures, Algorithms, and Web Development.
+       <p className="text-blue-500 font-customFont text-3xl -mt-20">
+       Aspiring Software Developer with a strong foundation in software engineering principles, currently pursuing a degree in Computer Science. Proficient in building data pipelines and automated ETL processes using technologies such as Kafka. Experienced in data visualization with tools like Tableau, Matplotlib, and JavaScript. Skilled in systems modeling and simulation using Python
         </p>
       </div>
     );
