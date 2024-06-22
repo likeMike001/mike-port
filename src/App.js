6272b213c3d3e066a934911033fcd0ea6b2b00a0
likeMike001 <like.mike.007.382@gmail.com>
2024-06-22 06:50:20 -0700

    added text in about comp and used framer - motion

--- src/App.js
+++ src/App.js
@@ -4,30 +4,29 @@ import About from './components/about/about.component';
 import Projects from './components/projects/projects.component';
 import Contact from './components/contact/contact.component';
 import References from './components/testimonial/testimonial.component';
-import ParticlesBackground from './components/particles/particles.background';
+
 
 function App() {
-  return (
 
+
+  return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-gray-400 via-gray-600 to-gray-800 relative">
-      <ParticlesBackground />
+
       <div className="w-full z-10">
         <Header />
-        <section id = "about">
+        <section id="about">
           <About />
         </section>
-        <section id = "projects">
+        <section id="projects">
           <Projects />
         </section>
-        <section id = "contact">
+        <section id="contact">
           <Contact />
         </section>
-        <section id = "references">
+        <section id="references">
           <References />
         </section>
-
-        <h3 className="text-2xl text-pink-500 mt-6 text-center">What do you see? </h3>
-        
+        <h3 className="text-2xl text-pink-500 mt-6 text-center">What do you see?</h3>
       </div>
     </div>
   );
--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -1,18 +1,39 @@
 import crwn from "../../assets/crown.jpg";
+import { motion } from "framer-motion";
 
 
 const About = () => {
     return (
-      <div className="flex items-center p-20 -mt-20">
+
+
+      <motion.div className="flex items-center p-20 -mt-20"
+        // animate = {{
+        //     scale : [1,2,2,1,1],
+        //     rotate: [0, 0, 270, 270, 0],
+        //     borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+        // }}
+        // transition={{
+        //     duration : 2,
+        //     ease :"easeOut",
+        // }}
+        drag
+        dragConstraints={{
+          top: -50,
+          left: -50,
+          right: 50,
+          bottom: 50,
+        }}
+    
+        >
         <img
           className="w-70 h-auto mr-4 "
           src={crwn}
           alt="Crown"
         />
-       <p className="text-blue-500 font-customFont text-3xl -mt-20">
+       <p className="text-pink-500 font-customFont text-3xl -mt-20">
        Aspiring Software Developer with a strong foundation in software engineering principles, currently pursuing a degree in Computer Science. Proficient in building data pipelines and automated ETL processes using technologies such as Kafka. Experienced in data visualization with tools like Tableau, Matplotlib, and JavaScript. Skilled in systems modeling and simulation using Python
         </p>
-      </div>
+      </motion.div>
     );
   }
   
