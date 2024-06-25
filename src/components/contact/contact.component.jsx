38e84554e5eb152e04580fdf9fa43dbf81d62f78
likeMike001 <like.mike.007.382@gmail.com>
2024-06-25 11:15:04 -0700

    finished the contact component

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -1,11 +1,24 @@
-
+import linkedIn from "../../assets/linkedin.svg";
+import leetCodeI from "../../assets/leetcode.svg";
+import gitHubI from "../../assets/github.svg";
 
 const Contact = () => {
   return (
-    <div className="p-10">
-      <h3 className="text-center font-customFont text-5xl" >Lets Connect !</h3>
+    <div className="p-10 text-center">
+      <h3 className="text-center font-customFont text-5xl">Let's Connect!</h3>
+      <div className="flex justify-center space-x-10 p-8">
+        <a href="https://www.linkedin.com/in/rudresh-bhandari-0817481aa/" target="_blank" rel="noopener noreferrer">
+          <img src={linkedIn} alt="LinkedIn" className="w-10 h-10" />
+        </a>
+        <a href="https://leetcode.com/u/LikeMike001/" target="_blank" rel="noopener noreferrer">
+          <img src={leetCodeI} alt="LeetCode" className="w-10 h-10" />
+        </a>
+        <a href="https://github.com/YourGitHubUsername" target="_blank" rel="noopener noreferrer">
+          <img src={gitHubI} alt="GitHub" className="w-10 h-10" />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
